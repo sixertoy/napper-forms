c304441649f0d9e902443f71070f55d7316f57e0
sixertoy <sixertoy.github@gmail.com>
2019-05-19 21:21:45 +0200

    add babel-eslint and yarn cli prepare command

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,24 @@
 import babel from 'rollup-plugin-babel';
 import commonJS from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-// import replace from 'rollup-plugin-replace';
 import { uglify } from 'rollup-plugin-uglify';
 
 import pkg from './package.json';
-
-const INPUT_FILE = 'src/index.js';
-const OUTPUR_CONFIG = {
-  // exports: 'named',
-  file: 'dist/bundle.min.js',
-  format: 'cjs',
-  // name: '@iziges/smarter-forms',
-};
 
 export default {
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
-  input: INPUT_FILE,
-  output: { ...OUTPUR_CONFIG },
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.min.js',
+    format: 'cjs',
+  },
   plugins: [
     resolve(),
     commonJS({ include: 'node_modules/**' }),
     babel({ exclude: 'node_modules/**', runtimeHelpers: true }),
-    // replace({ 'process.env.NODE_ENV': '"production"' }),
     uglify(),
   ],
 };
